511beb9ecc75ae0ec89b4afe6cdb06151cbb2522
Uladzislau Pravalenak <v-epam-UladzislauPra@racetrac.com>
2022-09-26 22:13:45 +0400

    Initial commit

---
+++ app/src/main/java/com/uladzislau/pravalenak/artpixabaysearch/presentation/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.uladzislau.pravalenak.artpixabaysearch.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uladzislau.pravalenak.artpixabaysearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
